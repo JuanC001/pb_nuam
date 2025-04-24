8eb7de933586ba097140f99cce92267959d2685c
Juan Camilo <jcflorezpaez@gmail.com>
2025-04-23 20:04:03 -0500

    feat: first commit

---
+++ src/main/java/com/nuam/pb_spring/pb_spring/PbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nuam.pb_spring.pb_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuam/pb_spring/pb_spring/PbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuam.pb_spring.pb_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
