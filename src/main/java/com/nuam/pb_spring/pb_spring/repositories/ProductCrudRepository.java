7e1035b90f033705940cf3333399332dbc0dea7a
Juan Camilo <jcflorezpaez@gmail.com>
2025-04-24 23:03:10 -0500

    feat: se crean repositorios para el producto

--- src/main/java/com/nuam/pb_spring/pb_spring/repositories/ProductCrudRepository.java
+++ src/main/java/com/nuam/pb_spring/pb_spring/repositories/ProductCrudRepository.java
@@ -2,8 +2,4 @@ package com.nuam.pb_spring.pb_spring.repositories;
 import com.nuam.pb_spring.pb_spring.models.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductCrudRepository extends CrudRepository<Product, Long> {
-
-
-
-}
+public interface ProductCrudRepository extends CrudRepository<Product, Long> {}
--- src/main/java/com/nuam/pb_spring/pb_spring/repositories/ProductRepository.java
+++ src/main/java/com/nuam/pb_spring/pb_spring/repositories/ProductRepository.java
@@ -1,21 +1,24 @@
 package com.nuam.pb_spring.pb_spring.repositories;
 
 import com.nuam.pb_spring.pb_spring.models.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductRepository {
 
+        @Autowired
         private ProductCrudRepository productCrudRepository;
 
         public List<Product> getAll() {
                 return (List<Product>) productCrudRepository.findAll();
         }
 
-        public Product getById(Long id) {
-                return productCrudRepository.findById(id).orElse(null);
+        public Optional<Product> getById(Long id) {
+                return productCrudRepository.findById(id);
         }
 
         public Product save(Product product) {
@@ -24,6 +27,10 @@ public class ProductRepository {
 
         public void delete(Long id) {
                 productCrudRepository.deleteById(id);
+        }
+
+        public Product update(Product product) {
+                return productCrudRepository.save(product);
         }
 
 }
